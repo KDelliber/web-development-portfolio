0bbc7ff6a3745457983f7c41795acd8b4568117e
KDelliber <118274479+KDelliber@users.noreply.github.com>
2026-08-24 21:05:14 -0400

    Add PowerShell service check script

---
+++ powershell/service-check.ps1
@@ -0,0 +1,23 @@
+# Service Check
+# Reviews a few services commonly relevant to workstation support.
+
+$services = @('Dnscache','Dhcp','Spooler','wuauserv')
+
+foreach ($name in $services) {
+    $svc = Get-Service -Name $name -ErrorAction SilentlyContinue
+    if ($svc) {
+        [PSCustomObject]@{
+            Service = $svc.DisplayName
+            Name = $svc.Name
+            Status = $svc.Status
+            StartType = $svc.StartType
+        }
+    } else {
+        [PSCustomObject]@{
+            Service = $name
+            Name = $name
+            Status = 'Not Found'
+            StartType = '-'
+        }
+    }
+} | Format-Table -AutoSize
